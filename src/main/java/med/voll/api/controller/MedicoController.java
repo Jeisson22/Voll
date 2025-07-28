bd9aae8810869824e2ac227e23472c426df99986
Jeisson22 <jeisson.15.ga@gmail.com>
2025-07-27 22:46:09 -0500

    Estandarizando retornos de la API

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,26 +34,36 @@ public class MedicoController {
        return repository.findAll(paginacion).stream().map(DatosListaMedico::new).toList();
     }*/
     @GetMapping
-    public Page<DatosListaMedico> listar(@PageableDefault(size=10, sort = {"nombre"}) Pageable  paginacion){
-        return repository.findAllByActivoTrue(paginacion).map(DatosListaMedico::new);
+    public ResponseEntity<Page<DatosListaMedico>> listar(@PageableDefault(size=10, sort = {"nombre"}) Pageable  paginacion){
+        var page =  repository.findAllByActivoTrue(paginacion).map(DatosListaMedico::new);
+
+        return  ResponseEntity.ok(page);
     }
 
 
     @Transactional
     @PutMapping
-    public void actualizar(@RequestBody @Valid DatosActualizacionMedico datos){
+    public ResponseEntity actualizar(@RequestBody @Valid DatosActualizacionMedico datos){
         // De esta forma obtenemos el medico dela base de datos por el id
         var medico = repository.getReferenceById(datos.id());
         // Ahora tenemos que crear un metodo para actualizar medico
         medico.actualizarInformaciones(datos);
+
+
+
+        return  ResponseEntity.ok(new DatosDetallesMedico(medico));
     }
+
+
 
     @Transactional
     @DeleteMapping("/{id}")
-    public void  eliminar(@PathVariable  Long id){
+    public ResponseEntity  eliminar(@PathVariable  Long id){
        var medico = repository.getReferenceById(id);
        medico.eliminar();
 
+
+       return ResponseEntity.noContent().build();
     }
 
 
